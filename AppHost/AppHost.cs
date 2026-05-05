272d1cf7e3c0dfd70685e38e884b1ac1da1d4d6c
Alexander Trauzzi <acj@trauzzi.me>
2026-05-05 08:56:46 -0500

    Vanity URL for local.

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -1,4 +1,5 @@
 using Aspire.Hosting;
+using Aspire.Hosting.Yarp;
 using Diagrid.Aspire.Hosting.Dashboard;
 
 var builder = DistributedApplication.CreateBuilder(args);
@@ -9,8 +10,20 @@ var password = builder.AddParameter("password", "zxczxc123", true);
 var postgres = builder.AddPostgres("postgres", username, password, port: 5432);
 var daprStateDatabase = postgres.AddDatabase("dapr-state", "dapr_state");
 
-builder
+var diagridDashboard = builder
     .AddDiagridDashboard()
     .WaitFor(daprStateDatabase);
+
+builder.AddYarp("proxy")
+    .WithHostPort(80)
+    .WaitFor(diagridDashboard)
+    .WithConfiguration((yarpConfiguration) =>
+    {
+        yarpConfiguration
+            .AddRoute(diagridDashboard.GetEndpoint("http"))
+            .WithMatchHosts("diagrid-dashboard.localhost")
+        ;
+    })
+;
 
 builder.Build().Run();
